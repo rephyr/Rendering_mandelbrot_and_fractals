0c7f4ef82f9cf511655dd4d87c96a5d16bc0cd6f
Elmeri Sipola <elmeri.sipola@tuni.fi>
2023-10-15 01:37:38 +0300

    Initial Commit

---
+++ Mandelbrot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Mandelbrot
+//
+//  Created by Elmeri Sipola on 15.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

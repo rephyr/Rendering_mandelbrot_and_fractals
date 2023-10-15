585ca39a146209fb24ad361c0f1d4e6e718c17c2
Elmeri Sipola <elmeri.sipola@tuni.fi>
2023-10-15 22:52:36 +0300

    Image rendering done.

--- Mandelbrot/main.cpp
+++ Mandelbrot/main.cpp
@@ -1,16 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <complex>
-#include <iostream>
 
 const int renderWidth = 1280;
 const int renderHeight = 720;
 
-int mandelbrotAlgo(std::complex<double> point){
+int mandelbrotAlgo(std::complex<double> point)
+{
+    // Bigger number == more accurate picture.
     const int maxIter = 250;
-    int iters = 0;
     std::complex<double> z(0,0);
 
-    for(iters; iters<maxIter; iters++)
+    for(int iters = 0; iters<maxIter; iters++)
     {
         z = z*z + point;
         if(abs(z)>2)
@@ -20,46 +20,49 @@ int mandelbrotAlgo(std::complex<double> point){
     }
     return maxIter;
 }
-void renderImage(){
+void renderImage(sf::Image& image) {
+    const float minX = -2.0f, maxX = 2.0f;  
+    const float minY = -2.0f, maxY = 2.0f;
+    const double scaledX = (maxX - minX) / renderWidth;  
+    const double scaledY = (maxY - minY) / renderHeight;
 
+    // Iterate through every pixel in our window
+    for (int pixelY = 0; pixelY < renderHeight; ++pixelY) {
+        for (int pixelX = 0; pixelX < renderWidth; ++pixelX) {
+            double xAxis = minX + pixelX * scaledX;
+            double yAxis = minY + pixelY * scaledY;  
+
+            std::complex<double> point(xAxis, yAxis);
+            int numIterations = mandelbrotAlgo(point);
+
+            // Colorpicking here
+        }
+    }
 }
-int main()
-{
+int main() {
     sf::RenderWindow window(sf::VideoMode(renderWidth, renderHeight), "Mandelbrot");
     sf::Texture texture;
     texture.create(renderWidth, renderHeight);
-    // Placeholder sprite while rendering the image
+    // Placeholder
     sf::Sprite sprite(texture);
-    // Assign x- and y-axis
-    const float minX = -2.0f, maxX = 2.0f;
-    const float minY = -2.0f, maxY = 2.0f;
-    // To scale the screen resolution to use desired axis
-    const float scaleX = (maxX - minX) / renderWidth;
-    const float scaleY = (maxY - minY) / renderHeight;
-    float xAxis;
-    float yAxis;
 
-    while (window.isOpen())
-    {
+    sf::Image image;
+    image.create(renderWidth, renderHeight, sf::Color::Black);
+
+    // Initial rendering of the image
+    renderImage(image);
+    texture.loadFromImage(image);
+    sprite.setTexture(texture, true);
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        sf::Image image;
-        image.create(renderWidth, renderHeight, sf::Color::Black);
-
-        // Iterate through every pixel in our window
-        for (int pixelX = 0; pixelX < renderWidth; ++pixelX)
-        {
-            for (int pixelY = 0; pixelY < renderHeight; ++pixelY)
-            {
-                xAxis = minX + pixelX * scaleX;
-                yAxis = maxY - pixelY * scaleY; 
-            }
-        }
+        window.clear();
+        window.draw(sprite);  // Draw the texture
         window.display();
     }
 
